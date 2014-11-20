735a2d5811eb5d5058932e16e5a68f0b3b1c3494
Zi Yu ZHAO <zzhao8@teaching.CS.McGill.CA>
2014-11-20 02:48:38 -0500

    Initial program

---
+++ Login.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<String.h>
+#include<ctype.h>
+
+
+int Verify(char* username, char* password){
+//This method takes in username and password and verify them according to the .csv file
+//finding name should be done by binary search
+
+}
+
+int main(int argc, char* argv[]){
+	
+
+
+}
