219d7da7138ce4b5f1be3bee8531b61c3a27c736
Zi Yu ZHAO <zzhao8@teaching.CS.McGill.CA>
2014-11-21 15:50:15 -0500

    updated version

--- Login.c
+++ Login.c
@@ -4,35 +4,25 @@
 #include<ctype.h>
 
 char*  getPW(FILE* acc,char* username){
-//Loop until eof, fgets each time. sscanf()to get username. clear buffer,strcmp
-//Return NULL if username!=exist, else return the password string
+//Loop until eof, fgets each time. strtok()to get username,strcmp()
 	char* userInfo=malloc(200);//records the line for a user: username,password 
-	//char* waste=malloc(100);
-	char* tempU=malloc(100);
 	char* tempP=malloc(100);
-	char* pReturn=malloc(100);
+	char* tempU=malloc(100);
 	do{
 		fgets(userInfo,200,acc);
 		tempU=strtok(userInfo,",");
-		tempP=strtok(NULL,",");
-		tempP=strtok(tempP,"\n");//get rid of the \n at the end of password
-		//printf("%d",strcmp(tempU,username));
-	/*	if(strcmp(tempU,username)==0){
-			strcpy(pReturn,tempP);
-			break;
-		}*/
+		if(strcmp(tempU,username)==0){
+			tempP=strtok(NULL,",");
+			return strtok(tempP,"\n");//need to remove \n from password
+		}
 	}	
-	while(!feof(acc)&&strcmp(tempU,username)!=0);	//while 1.not end of file 2.line isn't null 3.line doesn't matc
+	while(!feof(acc)&&strcmp(tempU,username)!=0);
 	return tempP;
 }
 int Verify(char* username, char* password){
 //This method takes in username and password and verify them according to the .csv file
 //finding name should be done by binary search
-	FILE* accounts=fopen("./admins.csv","rt");
-//need a function to read the textfile until the line with the given username, then return the password.
-	//need to determine if username exists. If yes, return the line.
-	//char* info=getUsr(accounts,username);//gets in a line user info
-	
+	FILE* accounts=fopen("./admins.csv","rt");	
 	char* pw;//Do not allow pw over 100 chars
 	char* tempPW=malloc(100);
 	tempPW=getPW(accounts,username);
@@ -40,7 +30,10 @@ int Verify(char* username, char* password){
 		pw=malloc(100);
 		strcpy(pw,tempPW);
 	}
-	else exit(EXIT_FAILURE);//still need error message-username not registered
+	else {
+		printf("Your username doesn't exist..");
+		exit(EXIT_FAILURE);//still need error message-username not registered
+	}
 	return strcmp(pw,password);
 }
 void verifySuccess(int result){
@@ -58,7 +51,7 @@ int main(int argc, char* argv[]){
 	char *un1="username";
 	char *pw1="password";
 	char *un2="arthur";
-	char *pw2="1zhao";
+	char *pw2="zhao";
 	int result=Verify(un2,pw2);//get result of verification
 	verifySuccess(result);
 	return 0;
