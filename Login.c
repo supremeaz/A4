f48707689a21b5ab65801a8179a15e243d931e2b
Zi Yu ZHAO <zzhao8@teaching.CS.McGill.CA>
2014-11-21 15:38:37 -0500

    Working functions. Just need to read input from website now

--- Login.c
+++ Login.c
@@ -7,20 +7,23 @@ char*  getPW(FILE* acc,char* username){
 //Loop until eof, fgets each time. sscanf()to get username. clear buffer,strcmp
 //Return NULL if username!=exist, else return the password string
 	char* userInfo=malloc(200);//records the line for a user: username,password 
-	char* waste=malloc(100);
+	//char* waste=malloc(100);
 	char* tempU=malloc(100);
+	char* tempP=malloc(100);
+	char* pReturn=malloc(100);
 	do{
 		fgets(userInfo,200,acc);
 		tempU=strtok(userInfo,",");
-		printf("%s",tempU);
-		if(strcmp(tempU,username)==0){
-		//	printf("%s",strtok(userInfo,","));
-			//return strtok(userInfo,",");//return the password
-		}
-		else waste=strtok(userInfo,",");//clear buffer
+		tempP=strtok(NULL,",");
+		tempP=strtok(tempP,"\n");//get rid of the \n at the end of password
+		//printf("%d",strcmp(tempU,username));
+	/*	if(strcmp(tempU,username)==0){
+			strcpy(pReturn,tempP);
+			break;
+		}*/
 	}	
-	while(!feof(acc));	//while 1.not end of file 2.line isn't null 3.line doesn't matc
-	return NULL;
+	while(!feof(acc)&&strcmp(tempU,username)!=0);	//while 1.not end of file 2.line isn't null 3.line doesn't matc
+	return tempP;
 }
 int Verify(char* username, char* password){
 //This method takes in username and password and verify them according to the .csv file
@@ -36,7 +39,6 @@ int Verify(char* username, char* password){
 	if(tempPW!=NULL){//-the case the username is registered
 		pw=malloc(100);
 		strcpy(pw,tempPW);
-		free(tempPW);
 	}
 	else exit(EXIT_FAILURE);//still need error message-username not registered
 	return strcmp(pw,password);
@@ -44,9 +46,9 @@ int Verify(char* username, char* password){
 void verifySuccess(int result){
 //Method takes in rsult of verification and act appropriately
 	if(result==0){
-		printf("The username and password matched!");
+		printf("The username and password matched!\n");
 	}
-	else printf("Your identification did not match, please try again");
+	else printf("Your identification did not match, please try again\n");
 }
 int main(int argc, char* argv[]){
 	//1-receive 2 strings from page
@@ -55,7 +57,9 @@ int main(int argc, char* argv[]){
 	/*-TESTS*/
 	char *un1="username";
 	char *pw1="password";
-	int result=Verify(un1,pw1);//get result of verification
+	char *un2="arthur";
+	char *pw2="1zhao";
+	int result=Verify(un2,pw2);//get result of verification
 	verifySuccess(result);
 	return 0;
 }
